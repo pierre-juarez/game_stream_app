e1f5230b8f70de3554131fee070216f351284053
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-23 23:33:19 -0500

    [add] Model JSON

--- GameStreamAppiOs/Models/Model.swift
+++ GameStreamAppiOs/Models/Model.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Games: Codable {
+  
+  let games:[Game]
+  
+}
+
+struct Game: Codable {
+  let title, studio, contentRating, publicationYear, description : String,
+      platforms, tags, galleryImages : [String],
+      videosUrl: videoUrl
+  
+}
+
+struct videoUrl: Codable {
+  let mobile, tablet : String
+}
